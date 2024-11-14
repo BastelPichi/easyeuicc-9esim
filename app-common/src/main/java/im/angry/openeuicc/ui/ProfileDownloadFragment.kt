089bd49aeab4bcc5f46e2c895b8b2afd17151832
septs <github@septs.pw>
2024-11-14 10:13:15 +0800

    chore: remove integers.xml

--- app-common/src/main/java/im/angry/openeuicc/ui/ProfileDownloadFragment.kt
+++ app-common/src/main/java/im/angry/openeuicc/ui/ProfileDownloadFragment.kt
@@ -32,6 +32,8 @@ class ProfileDownloadFragment : BaseMaterialDialogFragment(),
     companion object {
         const val TAG = "ProfileDownloadFragment"
 
+        const val NVRAM_THRESHOLD = 30 * 1024 // < 30 KiB, the alert may fail
+
         fun newInstance(slotId: Int, portId: Int, finishWhenDone: Boolean = false): ProfileDownloadFragment =
             newInstanceEuicc(ProfileDownloadFragment::class.java, slotId, portId) {
                 putBoolean("finishWhenDone", finishWhenDone)
@@ -135,8 +137,7 @@ class ProfileDownloadFragment : BaseMaterialDialogFragment(),
                 true
             }
             R.id.ok -> {
-                val threshold = resources.getInteger(R.integer.profile_download_nvram_threshold)
-                if (freeNvram > threshold) {
+                if (freeNvram > NVRAM_THRESHOLD) {
                     startDownloadProfile()
                 } else {
                     AlertDialog.Builder(requireContext()).apply {
