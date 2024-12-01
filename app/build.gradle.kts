37a3e2301868b127bc2464ccd0dca3d5d383122a
septs <github@septs.pw>
2024-12-01 10:52:45 +0800

    feat: per-app language preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,11 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    androidResources {
+        generateLocaleConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
