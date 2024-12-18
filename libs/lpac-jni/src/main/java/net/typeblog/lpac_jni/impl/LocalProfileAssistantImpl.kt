0fbec512ab7dd8be207bb771129a29eb5f9434a8
septs <github@septs.pw>
2024-12-18 21:27:53 +0800

    feat: atr in euiccinfo activity

--- libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/impl/LocalProfileAssistantImpl.kt
+++ libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/impl/LocalProfileAssistantImpl.kt
@@ -187,6 +187,8 @@ class LocalProfileAssistantImpl(
             return ret
         }
 
+    override fun readATR() = apduInterface.readATR()
+
     @Synchronized
     override fun enableProfile(iccid: String, refresh: Boolean): Boolean =
         LpacJni.es10cEnableProfile(contextHandle, iccid, refresh) == 0
