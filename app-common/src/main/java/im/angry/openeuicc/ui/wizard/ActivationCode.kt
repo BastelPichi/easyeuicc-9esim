321d9ca3f78216a9783ab4ea8294a8b149eb5dd6
septs <github@septs.pw>
2024-12-30 22:45:32 +0800

    chore: simplify logic

--- app-common/src/main/java/im/angry/openeuicc/ui/wizard/ActivationCode.kt
+++ app-common/src/main/java/im/angry/openeuicc/ui/wizard/ActivationCode.kt
@@ -8,26 +8,16 @@ data class ActivationCode(
     val requiredConfirmationCode: Boolean = false,
 ) {
     companion object {
-        private const val SCHEME = "LPA:"
-        private const val DELIMITER = '$'
-
         fun fromString(input: String): ActivationCode {
-            if (input.isBlank()) {
-                throw IllegalArgumentException("Activation code cannot be empty")
-            } else if (!input.startsWith(SCHEME)) {
+            val components = input.removePrefix("LPA:").split('$')
+            if (components.size < 2 || components[0] != "1") {
                 throw IllegalArgumentException("Invalid activation code format")
-            }
-            val components = input.removePrefix(SCHEME).split(DELIMITER)
-            if (components.size < 2) {
-                throw IllegalArgumentException("Invalid activation code format")
-            } else if (components[0] != "1") {
-                throw IllegalArgumentException("Invalid activation code AC_FORMAT")
             }
             return ActivationCode(
-                address = components[1],
-                matchingId = components.getOrNull(2),
-                oid = components.getOrNull(3),
-                requiredConfirmationCode = components.getOrNull(4) == "1"
+                address = components[1].trim(),
+                matchingId = components.getOrNull(2)?.trim()?.ifBlank { null },
+                oid = components.getOrNull(3)?.trim()?.ifBlank { null },
+                requiredConfirmationCode = components.getOrNull(4)?.trim() == "1"
             )
         }
     }
