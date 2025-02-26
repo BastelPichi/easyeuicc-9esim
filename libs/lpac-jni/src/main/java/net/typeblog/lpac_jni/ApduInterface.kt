3df506985c5a4715f44ff1d2f1dc6a06b4387f91
septs <github@septs.pw>
2025-02-26 21:32:04 +0800

    chore: accept reviews

--- libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/ApduInterface.kt
+++ libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/ApduInterface.kt
@@ -1,7 +1,5 @@
 package net.typeblog.lpac_jni
 
-import java.io.Closeable
-
 /*
  * Should reflect euicc_apdu_interface in lpac/euicc/interface.h
  */
@@ -19,26 +17,12 @@ interface ApduInterface {
      */
     val valid: Boolean
 
-    fun <T> withLogicalChannel(aid: ByteArray, callback: (ApduLogicalChannelHandle) -> T): T {
+    fun <T> withLogicalChannel(aid: ByteArray, cb: ((ByteArray) -> ByteArray) -> T): T {
         val handle = logicalChannelOpen(aid)
-        return ApduLogicalChannelHandle(handle, this).use(callback)
-    }
-}
-
-data class ApduLogicalChannelHandle(
-    private val handle: Int,
-    private val apduInterface: ApduInterface,
-) : Closeable {
-    private var closed: Boolean = false
-
-    fun transmit(tx: ByteArray) {
-        check(closed) { "Logical channel is already closed" }
-        apduInterface.transmit(handle, tx)
-    }
-
-    override fun close() {
-        if (closed) return
-        apduInterface.logicalChannelClose(handle)
-        closed = true
+        return try {
+            cb { transmit(handle, it) }
+        } finally {
+            logicalChannelClose(handle)
+        }
     }
 }
