3e183a9b2cca4d51697f8729071737dcf805ba54
septs <github@septs.pw>
2025-03-05 20:40:55 +0800

    feat: force use tmapi

--- app-common/src/main/java/im/angry/openeuicc/util/PreferenceUtils.kt
+++ app-common/src/main/java/im/angry/openeuicc/util/PreferenceUtils.kt
@@ -31,6 +31,7 @@ internal object PreferenceKeys {
 
     // ---- Developer Options ----
     val DEVELOPER_OPTIONS_ENABLED = booleanPreferencesKey("developer_options_enabled")
+    val REMOVABLE_TMAPI = booleanPreferencesKey("removable_tmapi")
     val UNFILTERED_PROFILE_LIST = booleanPreferencesKey("unfiltered_profile_list")
     val IGNORE_TLS_CERTIFICATE = booleanPreferencesKey("ignore_tls_certificate")
 }
@@ -48,6 +49,7 @@ class PreferenceRepository(private val context: Context) {
 
     // ---- Developer Options ----
     val developerOptionsEnabledFlow = bindFlow(PreferenceKeys.DEVELOPER_OPTIONS_ENABLED, false)
+    val removableTMAPIFlow = bindFlow(PreferenceKeys.REMOVABLE_TMAPI, false)
     val unfilteredProfileListFlow = bindFlow(PreferenceKeys.UNFILTERED_PROFILE_LIST, false)
     val ignoreTLSCertificateFlow = bindFlow(PreferenceKeys.IGNORE_TLS_CERTIFICATE, false)
 
