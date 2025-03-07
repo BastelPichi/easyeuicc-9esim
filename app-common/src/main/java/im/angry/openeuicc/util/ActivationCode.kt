cd81589691eee7b7da6288fa5dacf13ea6ed2ec2
septs <github@septs.pw>
2025-03-07 15:38:15 +0800

    fix: exception

--- app-common/src/main/java/im/angry/openeuicc/util/ActivationCode.kt
+++ app-common/src/main/java/im/angry/openeuicc/util/ActivationCode.kt
@@ -11,7 +11,7 @@ data class ActivationCode(
             val components = input.removePrefix("LPA:").split('$')
                 .map(String::trim).map { it.ifBlank { null } }
             if (components.size < 2 || components[0] != "1" || components[1] == null) {
-                throw IllegalArgumentException("Invalid activation code format")
+                throw IllegalStateException("Invalid activation code format")
             }
             return ActivationCode(
                 components[1]!!,
